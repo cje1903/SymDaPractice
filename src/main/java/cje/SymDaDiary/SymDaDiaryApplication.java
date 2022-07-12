44b006bdd5feb7cecba1cf838bbea68bbd1e7996
cje1903 <cje1903@naver.com>
2022-07-13 02:42:43 +0900

    init project

---
+++ src/main/java/cje/SymDaDiary/SymDaDiaryApplication.java
@@ -0,0 +1,13 @@
+package cje.SymDaDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymDaDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymDaDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cje/SymDaDiary/SymDaDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package cje.SymDaDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymDaDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
